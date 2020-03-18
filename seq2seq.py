b487fd8c73b1425873cf34a4aadf302b84c76d8a
Matej Klemen <matej.klemen1337@gmail.com>
2020-03-18 11:19:45 +0100

    Add support for bidirectional LSTM

--- seq2seq.py
+++ seq2seq.py
@@ -14,15 +14,22 @@ class ResidualLSTMEncoder(nn.Module):
             for layer_id in residual_layers:
                 self.is_res[layer_id] = True
 
+        # internally, each direction will produce inp_hid_size/2 features, which will get concatenated back together
+        if bidirectional and inp_hid_size % 2 != 0:
+            raise ValueError("Hidden state size must be even")
+        self.bidirectional = bidirectional
+        num_directions = 2 if self.bidirectional else 1
+
         self.input_size = inp_hid_size
-        self.hidden_size = inp_hid_size
+        self.hidden_size = inp_hid_size // num_directions
         self.num_layers = num_layers
         self.dropout = nn.Dropout(p=dropout)
         self.embeddings = nn.Embedding(num_embeddings=vocab_size,
-                                       embedding_dim=inp_hid_size)
-        self.layers = nn.ModuleList([nn.LSTM(input_size=inp_hid_size,
-                                             hidden_size=inp_hid_size,
-                                             batch_first=True) for _ in range(num_layers)])
+                                       embedding_dim=self.input_size)
+        self.layers = nn.ModuleList([nn.LSTM(input_size=self.input_size,
+                                             hidden_size=self.hidden_size,
+                                             batch_first=True,
+                                             bidirectional=self.bidirectional) for _ in range(num_layers)])
 
     def forward(self, encoded_seq):
         """
@@ -42,6 +49,8 @@ class ResidualLSTMEncoder(nn.Module):
             [2] list of last cell states for each layer
                 (num_layers * (1, batch_size, hidden_size) tensors)
         """
+        batch_size, max_seq_len = encoded_seq.shape
+        num_directions = 2 if self.bidirectional else 1
         embedded = self.dropout(self.embeddings(encoded_seq))
 
         last_t_hids, last_t_cells = [], []
@@ -49,6 +58,16 @@ class ResidualLSTMEncoder(nn.Module):
         curr_inp = embedded
         for i, curr_lstm in enumerate(self.layers):
             curr_out, (curr_hid, curr_cell) = curr_lstm(curr_inp)
+            # combine features for both directions
+            if self.bidirectional:
+                out_by_dir = curr_out.view(batch_size, max_seq_len, num_directions, self.hidden_size)
+                hid_by_dir = curr_hid.view(1, num_directions, batch_size, self.hidden_size)
+                cell_by_dir = curr_cell.view(1, num_directions, batch_size, self.hidden_size)
+
+                curr_out = torch.cat((out_by_dir[:, :, 0, :], out_by_dir[:, :, 1, :]), dim=2)
+                curr_hid = torch.cat((hid_by_dir[:, 0, :, :], hid_by_dir[:, 1, :, :]), dim=2)
+                curr_cell = torch.cat((cell_by_dir[:, 0, :, :], cell_by_dir[:, 1, :, :]), dim=2)
+
             last_layer_hids = curr_out
             if self.is_res[i]:
                 curr_inp = curr_out + embedded
@@ -89,9 +108,9 @@ class ResidualLSTMDecoder(nn.Module):
             raise ValueError(f"Valid options for 'num_attn_layers': 0 or 1 or {num_layers} (= num. LSTM layers)")
         self.num_attn_layers = num_attn_layers
 
-        # LSTMs will now get 2 things as input:
+        # LSTMs will get 2 things as input (IF using attention):
         # [attended encoder states, embedded decoder input/hidden state] (concatenated)
-        self.layers = nn.ModuleList([nn.LSTM(input_size=(inp_size + hid_size),
+        self.layers = nn.ModuleList([nn.LSTM(input_size=(inp_size + hid_size) if num_attn_layers > 0 else inp_size,
                                              hidden_size=hid_size,
                                              batch_first=True) for _ in range(num_layers)])
         self.fc = nn.Linear(hid_size, vocab_size)
